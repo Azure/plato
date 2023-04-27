58ee5f77442996c4c0cf2e88075a8f9713a9a6fe
jillianmclements <73854757+jillianmclements@users.noreply.github.com>
2023-04-27 10:34:09 -0400

    modify deploy-agent to run without a registered sim env (#26)

--- examples/deploy-agent/src/serve.py
+++ examples/deploy-agent/src/serve.py
@@ -1,26 +1,66 @@
+import pickle
 from pathlib import Path
 
+import gymnasium.spaces as spaces
 import numpy as np
 from ray import serve
-from ray.rllib.algorithms.algorithm import Algorithm
-from ray.tune.registry import register_env
-
-# IMPORTANT: Check that the sim is available as sim.py in the src folder
-from sim import SimpleAdder as SimEnv
 
 # The name of the folder where the checkpoints are located in the checkpoints/ folder
 # IMPORTANT: Remember to change it
 CHECKPOINT_FOLDER = "checkpoint_000010"
 
-# IMPORTANT: sim_env must be equal to the name used to register the environment when
-# training the agent.
-register_env("sim_env", lambda conf: SimEnv(conf))
+# The observation and action spaces defined in your sim env
+# IMPORTANT: Remember to change them
+observation_space = spaces.Dict(
+    {"value": spaces.Box(low=-float("inf"), high=float("inf"))}
+)
+action_space = spaces.Dict({"addend": spaces.Box(low=-10, high=10, dtype=np.int32)})
 
 
-# IMPORTANT: Modify the following function
-def prepare_state(state):
-    """Transform the state into the form expected by the agent."""
-    return {"value": np.array([state["value"]])}
+def check_and_transform(observation_space, state):
+    """Check and transform a observation state instance for a given observation space.
+
+    This function takes a state instance and an observation space object that defines
+    the valid shape and dtype of the state data for the simulation environment. The
+    function then checks if the state instance is already valid for the observation
+    space, and if not, it tries to transform it into a valid form.
+
+    Args:
+        observation_space (gym.Space): The observation space of the sim env.
+        state (JSON): The state instance to check and transform.
+
+    Returns:
+        state: The transformed state instance that is valid for the observation space.
+
+    Raises:
+        ValueError: If the state instance cannot be transformed for the observation
+        space.
+    """
+    # Check if the instance is valid for the space
+    if isinstance(state, np.ndarray) and observation_space.contains(state):
+        # Return the instance as it is
+        return state
+    # Try to transform the instance based on the space type
+    elif isinstance(observation_space, spaces.Box):
+        # Convert the instance to a numpy array with the same
+        # shape and dtype as the space
+        return np.array(state, dtype=observation_space.dtype).reshape(
+            observation_space.shape
+        )
+    elif isinstance(observation_space, spaces.Discrete):
+        # Convert the instance to an integer
+        return int(state)
+    elif isinstance(observation_space, spaces.Dict):
+        # Convert the instance to a dictionary with valid values for each
+        # key in the space
+        return {
+            key: check_and_transform(observation_space.spaces[key], state[key])
+            for key in observation_space.spaces
+        }
+    # Add more cases for other types of spaces as needed
+    else:
+        # Raise an exception if the instance cannot be transformed
+        raise ValueError(f"Cannot transform {state} for {observation_space}")
 
 
 def prepare_path(checkpoint_folder):
@@ -29,15 +69,47 @@ def prepare_path(checkpoint_folder):
     )
 
 
+def prepare_agent(checkpoint_folder):
+    """Prepare an RLlib agent for inference from a checkpoint folder.
+
+    This function loads the RLlib AlgorithmConfig from the checkpoint folder,
+    unfreezes it to make changes to the environment, rebuilds the algorithm, and
+    restores the algorithm state from the checkpoint to instantiate the agent.
+
+    Args:
+        checkpoint_folder (str): The path to the checkpoint folder.
+
+    Returns:
+        Algorithm: The prepared algorithm for inference.
+    """
+    # Load the algorithm information from the checkpoint
+    with open(checkpoint_folder + "/algorithm_state.pkl", "rb") as f:
+        data = pickle.load(f)
+
+    # Unfreeze the AlgorithmConfig to make changes to env
+    algo_config = data["config"].copy(copy_frozen=False)
+
+    # Change the sim env to None (not needed for inference), and rebuild the algorithm
+    algo = algo_config.environment(
+        env=None,
+        observation_space=observation_space,
+        action_space=action_space,
+    ).build()
+
+    # Restore the algorithm state from the checkpoint
+    algo.restore(checkpoint_folder)
+    return algo
+
+
 @serve.deployment
 class ServeAgent:
     def __init__(self):
         check_path = prepare_path(CHECKPOINT_FOLDER)
-        self.agent = Algorithm.from_checkpoint(check_path)
+        self.agent = prepare_agent(check_path)
 
     async def __call__(self, request):
         json_input = await request.json()
-        state = prepare_state(json_input["state"])
+        state = check_and_transform(observation_space, json_input["state"])
         # Set explore to false or the agent will not be deterministic
         action = self.agent.compute_single_action(state, explore=False)
         return action
