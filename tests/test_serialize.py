3d36b711f01a9847d1c158bfb32e6f92f7a77969
Marco Zatta <marco.zatta@microsoft.com>
2023-05-24 09:56:32 +0200

    Add serialize utils to platotk
    Move some functions useful for serialization/deserialization into a module
    inside the platotk package.

--- tests/test_serialize.py
+++ tests/test_serialize.py
@@ -1,7 +1,24 @@
+import json
+
 import numpy as np
 import pytest
 from gymnasium import spaces
-from src.serve import check_and_transform
+
+from platotk.serialize import GymEncoder, check_and_transform
+
+states = (
+    ({"float": np.float_(90)}, '{"float": 90.0}'),
+    ({"int": np.int_(90)}, '{"int": 90}'),
+    ({"bool": np.bool_(1)}, '{"bool": true}'),
+    ({"list_float": np.arange(2, dtype=np.float_)}, '{"list_float": [0.0, 1.0]}'),
+    ({"list_int": np.arange(2, dtype=np.int_)}, '{"list_int": [0, 1]}'),
+)
+
+
+@pytest.mark.parametrize("state, encoded", states)
+def test_numpy_encoder(state, encoded):
+    assert json.dumps(state, cls=GymEncoder) == encoded
+
 
 samples = [
     # Test a valid instance for a Discrete space
