5cf0237fae96173e95534f5fc5d3635a6ee514f4
Marco Zatta <marco.zatta@microsoft.com>
2023-08-07 12:31:12 +0200

    Modify sim-baobab connector to allow for parallelization
    When initiating a new sim environment, we create also a baobab instance with
    unique namespace for environment.
    This allows to properly run multiple sim in parallel.

--- examples/getting-started-anylogic-sim/src/start.sh
+++ examples/getting-started-anylogic-sim/src/start.sh
@@ -2,9 +2,4 @@
 set -xe
 memcached -p 11211 -u memcache -l 127.0.0.1 -d
 
-gunicorn \
-	--worker-class uvicorn.workers.UvicornWorker \
-	--bind '127.0.0.1:8000' \
-	platotk.baobab:app &
-
 python -u main.py --test-local
